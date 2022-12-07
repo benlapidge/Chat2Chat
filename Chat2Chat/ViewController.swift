ceffbf9c95b4f531b9b1cd47e2f511446f0649e0
benlapidge <81932153+benlapidge@users.noreply.github.com>
2022-12-07 18:39:53 +0000

    Initial Commit

---
+++ Chat2Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat2Chat
+//
+//  Created by Ben Lapidge on 07/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
